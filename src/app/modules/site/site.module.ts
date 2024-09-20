04096491c462c02c563d0c4bf756e54bdb60e0d3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-20 14:56:20 -0300

    chore: site module

--- src/app/modules/site/site.module.ts
+++ src/app/modules/site/site.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SiteModule } from './site/site.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    SiteModule
+    CommonModule
   ]
 })
-export class BaseModule { }
+export class SiteModule { }
